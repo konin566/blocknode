6c8685f3f6941a3ae4bb242a0f48d2a92df50d79
konin miraz <44706348+konin566@users.noreply.github.com>
2018-12-07 01:44:51 +0600

    Revert "Fix testnet sync issue "

--- src/pow.cpp
+++ src/pow.cpp
@@ -39,10 +39,8 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
         uint256 bnTargetLimit = (~uint256(0) >> 24);
         int64_t nTargetSpacing = 60;
         int64_t nTargetTimespan = 60 * 40;
-
-        if (IsSporkActive(SPORK_17_NEW_PROTOCOL_TARGETTIME) &&
-                ((Params().NetworkID() == CBaseChainParams::MAIN && pindexLast->nHeight > 10615) ||
-                 (Params().NetworkID() == CBaseChainParams::TESTNET && pindexLast->nHeight > 55119))) {
+        if (IsSporkActive(SPORK_17_NEW_PROTOCOL_TARGETTIME) && 
+            pindexLast->nHeight > 10615) {
             nTargetSpacing = Params().TargetSpacing();
             nTargetTimespan = Params().TargetTimespan();;
         }
